f3c0bef34cdf4ccdfcb8d8af4c554dff7d4421e3
Wander Amaral <wanderamaral013@gmail.com>
2024-04-15 18:09:56 -0300

    feat: add types

---
+++ src/app/app/(main)/actions.ts
@@ -0,0 +1,14 @@
+"use server";
+import { auth } from "@/services/auth";
+import { prisma } from "@/services/database";
+
+export async function getUserTodos() {
+  const session = await auth();
+
+  const todos = await prisma.todo.findMany({
+    where: {
+      userId: session?.user?.id,
+    },
+  });
+  return todos;
+}
---
+++ src/app/app/(main)/types.ts
@@ -0,0 +1,4 @@
+import { ReturnTypeWithoutPromise } from "@/app/types/return-type-without-promise";
+import { getUserTodos } from "./actions";
+
+export type Todo = ReturnTypeWithoutPromise<typeof getUserTodos>[0]
