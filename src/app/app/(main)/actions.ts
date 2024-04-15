69ff1bd19c34d7b32093d766270275975f0a7eeb
Wander Amaral <wanderamaral013@gmail.com>
2024-04-15 19:25:50 -0300

    feat: created scheme with types

--- src/app/app/(main)/actions.ts
+++ src/app/app/(main)/actions.ts
@@ -1,6 +1,9 @@
-"use server";
+"use server"
+import { upsertTodoSchema } from "./schema";
 import { auth } from "@/services/auth";
 import { prisma } from "@/services/database";
+import { error } from "console";
+import { z } from "zod";
 
 export async function getUserTodos() {
   const session = await auth();
@@ -9,6 +12,71 @@ export async function getUserTodos() {
     where: {
       userId: session?.user?.id,
     },
+    orderBy: {
+      createdAt: "desc"
+    }
   });
   return todos;
+}
+
+export async function upsertTodo(input: z.infer<typeof upsertTodoSchema>) {
+  const session = await auth()
+
+  if (!session?.user?.id) {
+    return {
+      error: 'Not authorized',
+      data: null,
+    }
+  }
+
+  if (input.id) {
+    const todo = await prisma.todo.findUnique({
+      where: {
+        id: input.id,
+        userId: session?.user?.id,
+      },
+      select: {
+        id: true,
+      },
+    })
+
+    if (!todo) {
+      return {
+        error: 'Not found',
+        data: null,
+      }
+    }
+
+    const updatedTodo = await prisma.todo.update({
+      where: {
+        id: input.id,
+        userId: session?.user?.id,
+      },
+      data: {
+        title: input.title,
+        doneAt: input.doneAt,
+      },
+    })
+
+    return {
+      error: null,
+      data: updatedTodo,
+    }
+  }
+
+  if (!input.title) {
+    return {
+      error: 'Title is required',
+      data: null,
+    }
+  }
+
+  const todo = await prisma.todo.create({
+    data: {
+      title: input.title,
+      userId: session?.user?.id,
+    },
+  })
+
+  return todo
 }
