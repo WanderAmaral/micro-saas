421e6675b4368ec8aa33ee42ab10887c0e82c2e7
Wander Amaral <wanderamaral013@gmail.com>
2024-04-15 19:26:13 -0300

    feat: add function of server action in component dialog

--- src/app/app/(main)/_components/dialog-add-task.tsx
+++ src/app/app/(main)/_components/dialog-add-task.tsx
@@ -20,6 +20,12 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Todo } from "../types";
+import { upsertTodo } from "../actions";
+import { useRef } from "react";
+import { upsertTodoSchema } from "../schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { toast } from "@/components/ui/use-toast";
 
 interface DialogAddTaskProps {
   children?: React.ReactNode;
@@ -27,18 +33,33 @@ interface DialogAddTaskProps {
 }
 
 const DialogAddTask = ({ children }: DialogAddTaskProps) => {
-  const form = useForm();
+  const ref = useRef<HTMLDivElement>(null);
+  const form = useForm({
+    resolver: zodResolver(upsertTodoSchema),
+  });
+  const router = useRouter();
 
-  const handleOnSubmit = form.handleSubmit((data) => {
-    console.log({ data });
+  const handleOnSubmit = form.handleSubmit(async (data) => {
+    await upsertTodo(data);
+    router.refresh();
+
+    ref.current?.click();
+
+    toast({
+      title: "Sucess",
+      description: "Your todo has been updates succesfully",
+    });
   });
 
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline" className="ml-auto">
+        <div
+          className="font-medium border rounded-md w-[130px] flex items-center justify-center hover:bg-black hover:text-white cursor-pointer"
+          ref={ref}
+        >
           {children}
-        </Button>
+        </div>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>Add Task</DialogHeader>
@@ -46,21 +67,23 @@ const DialogAddTask = ({ children }: DialogAddTaskProps) => {
           <form className="space-y-8" onSubmit={handleOnSubmit}>
             <FormField
               control={form.control}
-              name="username"
+              name="title"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Name task</FormLabel>
+                  <FormLabel>Title</FormLabel>
                   <FormControl>
-                    <Input placeholder="Task" {...field} />
+                    <Input placeholder="Enter your todo title" {...field} />
                   </FormControl>
                   <FormDescription>
-                    This is your public display name.
+                    This will be the publicly displayed name for the task.
                   </FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
             />
-            <Button type="submit" variant={'outline'}>Submit</Button>
+            <Button type="submit" variant={"outline"}>
+              Submit
+            </Button>
           </form>
         </Form>
       </DialogContent>
--- src/app/app/(main)/_components/todo-data-table.tsx
+++ src/app/app/(main)/_components/todo-data-table.tsx
@@ -20,7 +20,6 @@ import {
 } from "@tanstack/react-table";
 
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
